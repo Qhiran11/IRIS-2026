f64c0bb51cce71cc7cdc6c32c39cfbe754af7334
Qhiran11 <muhammadqhiran@gmail.com>
2026-05-25 20:31:11 +0800

    Heii ^_^ | kapan ketemu ? Aku Rindu

--- InputTombol.py
+++ InputTombol.py
@@ -6,7 +6,7 @@ class MatrixKeypad:
         # 1. Tentukan Pin (Gunakan penomoran BOARD/Fisik pada Jetson Nano)
         # Pastikan pin ini aman dan tidak dipakai oleh I2C/SPI tanpa sengaja
         self.ROW_PINS = [31, 13, 11, 7]    # 4 Pin untuk Baris (R1, R2, R3, R4)
-        self.COL_PINS = [33, 35, 37]
+        self.COL_PINS = [37, 35, 35]
 
         # 2. Pemetaan Layout Tombol 
         # Sesuaikan dengan bentuk fisik D-Pad / Keypad Anda
